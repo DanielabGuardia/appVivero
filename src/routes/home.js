fa50b03912f8d78c1f18d9925f9f8386416aac82
DanielabGuardia <danielabguardia@gmail.com>
2022-07-27 15:53:35 -0300

    setting

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,8 +3,8 @@ let router = express.Router()
 let controller = require('../controllers/homeController')
 
 router.get('/', controller.home)
-router.get('/frequent-questions', controller.preguntas_frecuentes)
-router.get('/locals', controller.locals)
+/*router.get('/frequent-questions', controller.preguntas_frecuentes)
+router.get('/locals', controller.locals)*/
 
 
 module.exports = router
